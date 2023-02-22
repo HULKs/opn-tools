cd1663fa7b93c25705bb4df11eb95929c0e57985
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-02-22 22:16:25 +0100

    Readd overlay file system
    Co-authored-by: Maximilian Schmidt <maximilian.schmidt@tuhh.de>

--- mkopn/installer.sh
+++ mkopn/installer.sh
@@ -23,39 +23,12 @@ sync
 MOUNTED_FILE_SYSTEMS=$(mount | grep '^/dev' | cut -d' ' -f3)
 [ -n "${MOUNTED_FILE_SYSTEMS}" ] && (umount -fr "${MOUNTED_FILE_SYSTEMS}" && sync && sleep 2)
 
-# recreate partitions
-(
-  # print the partition table
-  echo p
-  # delete /dev/mmcblk0p4 partition
-  echo d
-  echo 4
-  # delete /dev/mmcblk0p3 partition
-  echo d
-  echo 3
-  # add new /dev/mmcblk0p3 partition
-  echo n
-  echo 3
-  echo
-  echo +27G
-  # add new /dev/mmcblk0p4 partition
-  echo n
-  echo 4
-  echo
-  echo
-  # print the partition table
-  echo p
-  echo w
-) | fdisk /dev/mmcblk0
-sfdisk --part-uuid /dev/mmcblk0 3 42424242-1120-1120-1120-424242424242
-sfdisk --part-uuid /dev/mmcblk0 4 66666666-1120-1120-1120-666666666666
-
 # provision data
-mkfs.ext2 -q /dev/mmcblk0p4
+mkfs.ext2 -U 66666666-1120-1120-1120-666666666666 -L data -q /dev/mmcblk0p4
 mkdir -p /data
 mount /dev/mmcblk0p4 /data
-mkdir -p /data/.image
-chown 1001:1001 /data/.image
+mkdir -p /data/home/nao /data/.work-home-nao /data/.image
+chown 1001:1001 /data/home/nao /data/.image
 umount /data
 
 # disable image
